9f4fe1862eb3a05e46b6af615c04c4b889565350
Esaú Zepeta Castillo <31966614+LEOVimOpen@users.noreply.github.com>
2017-09-21 21:31:31 -0500

    Create tabla-de-3.c

---
+++ Tarea 8/tabla-de-3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int m=0;
+    int n=0;
+    system("clear");
+    printf("Este programa imprime la tabla del 3\n\n");
+    for(m=1;m<=10;m++){
+        printf("3 x %i = %i \n",m,m*3);
+    }
+}
