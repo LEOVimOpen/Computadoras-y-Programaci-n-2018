05a3eff779d28ddb086a549f241c3b6b84defa47
Esaú Zepeta Castillo <31966614+LEOVimOpen@users.noreply.github.com>
2017-09-21 20:38:59 -0500

    Create hola.c

---
+++ Tarea 8/hola.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int m=0;
+    int n=0;
+    system("clear");
+    printf("Este programa te saluda 10 veces\n\n");
+    for(m=1;m<=10;m++){
+        printf("%i Hola\n",m);
+    }
+}
